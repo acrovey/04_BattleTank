f6fbd68b0ed13365a52b67086f5ec7da80c8f4cc
MONOLITH\Croveski <acrovey@gmail.com>
2017-07-18 16:21:18 -0500

    Added Blank Landscape

---
+++ BattleTankGame/Source/BattleTankGame/BattleTankGame.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTankGame.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleTankGame, "BattleTankGame" );
